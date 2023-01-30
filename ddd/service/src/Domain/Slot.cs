94251ca1f15a14b81b91b80c3cda7c80b62ee162
Edson Carli <gandnegro@gmail.com>
2023-01-30 09:04:31 +0000

    ddd

---
+++ ddd/service/src/Domain/Slot.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain;
+
+public class Slot : Entity
+{
+    public Slot(
+        SnackMachine snackMachine,
+        int position)
+    {
+        Position = position;
+        SnackMachine = snackMachine;
+        SnackPile = new SnackPile(null, 0, 0m);
+    }
+
+    private Slot()
+    {
+    }
+
+    public int Position { get; }
+    public SnackMachine SnackMachine { get; }
+    public SnackPile SnackPile { get; set; }
+}
