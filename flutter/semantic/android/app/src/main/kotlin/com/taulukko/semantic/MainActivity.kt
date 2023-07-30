669f18b399ccc1bf5f2ff72285769bc92973d0b1
Edson Carli <gandnegro@gmail.com>
2023-07-30 10:32:11 +0100

    flutter semantic examples

---
+++ flutter/semantic/android/app/src/main/kotlin/com/taulukko/semantic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.taulukko.semantic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/semantic/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // this should be called after RendererBinding.instance?.setSemanticsEnabled(true);
+
+  // Auto-enable accessibility for our Blind and Low Vision customers (see
+  // https://docs.flutter.dev/development/accessibility-and-localization/accessibility#screen-readers).
+  SemanticsBinding.instance.ensureSemantics();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // showSemanticsDebugger: true,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Semantics(
+            value: "", excludeSemantics: true, child: Text(widget.title)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Semantics(
+        value: "Aumentar um",
+        button: true,
+        excludeSemantics: true,
+        child: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add)),
+      ),
+    );
+  }
+}
