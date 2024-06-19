8096a2f7a0c6f479c4dec3bdf4c30c3e47fccd22
Edson Carli <gandnegro@gmail.com>
2024-06-19 06:52:26 +0100

    node com typescript usando import

---
+++ javascript/node/typescript-com-imports/src/index.ts
@@ -0,0 +1,10 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Agora você pode usar __dirname
+const pathToSomeFile = join(__dirname, 'somefile.txt');
+
+console.log(`O caminho para o arquivo é: ${pathToSomeFile}`);
