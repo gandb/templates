91de914fe272f177ef0eb5b3dcf2a02dfcf7148e
gandb <gandnegro@gmail.com>
2025-04-26 01:43:24 +0100

    correcao do user script millenium

--- tampermonkey/auto code millenium.user.js
+++ tampermonkey/auto code millenium.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         auto code millenium
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
-// @match        https://ind.millenniumbcp.pt/*
+// @match        https://www.millenniumbcp.pt/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=millenniumbcp.pt
 // @grant        none
 // ==/UserScript==
@@ -14,7 +14,17 @@ const CODIGO_UTILIZADOR = 'FVCJ120704';
 (function() {
     'use strict';
 
-    // Your code here...
-    let iUserCode = document.getElementById('iUserCode');
-    iUserCode.value = CODIGO_UTILIZADOR;
+    // Função para verificar se o campo está visível e preencher
+    function preencherCampoQuandoVisivel() {
+        let iUserCode = document.getElementById('inptCode');
+
+        // Verificar se o campo existe e está visível
+        if (iUserCode && iUserCode.offsetParent !== null) {
+            iUserCode.value = CODIGO_UTILIZADOR;
+            clearInterval(intervalo); // Parar o intervalo quando o campo for preenchido
+        }
+    }
+
+    // Intervalo para verificar a cada 500ms
+    let intervalo = setInterval(preencherCampoQuandoVisivel, 500);
 })();
