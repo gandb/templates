9ad9dc138f720f77279a092a32699f6a38fb6e17
gandb <gandnegro@gmail.com>
2026-03-05 07:59:41 +0000

    devcontainer com visualstudio e docker

---
+++ docker/visualstudio-code/index.ts
@@ -0,0 +1,4 @@
+//Além do docker estar rodando (claro), é necessário compartilhar (config/resources/filesharing) com o local de trabalho, no meu caso /media/gandb/workspace/
+//No visual studio code em procurar digite > e depois >Reabrir no container (ou Reopen in Containner)
+//Para voltar localmente, basta escrever >Reabrir Localmente
+console.log("Hello Docker World");
